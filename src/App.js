4188c3bf68dd7a402a5308036da3230016086c1b
Ekene Ejeh <ekeneejeh@gmail.com>
2022-04-18 12:17:01 -0700

    trying diffrent Material UI 5 Component styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,67 @@
+import { Button, Typography } from "@mui/material";
+
+import { SettingsPower, Settings, Add, Pets, Clear } from "@mui/icons-material";
+
 function App() {
-	return <div>Hello World!</div>;
+	return (
+		<>
+			<Button variant="text">Text</Button>
+			<Button startIcon={<Settings />} variant="contained">
+				Contained
+			</Button>
+			<Button
+				startIcon={<SettingsPower />}
+				variant="contained"
+				size="small"
+				color="success"
+			>
+				Power
+			</Button>
+			<Button startIcon={<Add />} variant="text" size="small" color="success">
+				Add New Post
+			</Button>
+			<Button
+				startIcon={<Pets />}
+				variant="contained"
+				size="small"
+				color="secondary"
+			>
+				Pets
+			</Button>
+			<Button variant="outlined" disabled>
+				disabled
+			</Button>
+			<Button
+				startIcon={<Clear />}
+				variant="outlined"
+				size="small"
+				color="error"
+			>
+				Delete
+			</Button>
+			<Typography variant="h1" component="p">
+				Paragraph notes here
+			</Typography>
+			<Button
+				variant="contained"
+				disabled
+				sx={{
+					backgroundColor: "skyblue",
+					color: "primary",
+					margin: 5,
+					"&:hover": {
+						backgroundColor: "lightblue",
+					},
+					"&:disabled": {
+						backgroundColor: "grey",
+						color: "white",
+					},
+				}}
+			>
+				Strange Button
+			</Button>
+		</>
+	);
 }
 
 export default App;
